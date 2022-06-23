4adf71585b7669c6717dbc14f4d5fcc96cf67269
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2022-06-23 10:24:12 -0400

    useInput hook

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,8 +1,29 @@
 import React, { useState, useRef, useEffect } from "react";
+import useInput from "../hooks/use-input";
 
 const SimpleInput = (props) => {
   // const nameInputRef = useRef();
-  const [enteredName, setEnteredName] = useState("");
+  const {
+    value: enteredName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError,
+    valueChangeHandler: nameInputChangeHandler,
+    inputBlurHandler: nameInputBlurHandler,
+    reset: resetNameInput,
+  } = useInput((value) => value.trim() !== "");
+
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailInputHasError,
+    valueChangeHandler: emailInputChangeHandler,
+    inputBlurHandler: emailInputBlurHandler,
+    reset: resetEmailInput,
+  } = useInput((value) => value.trim() !== "");
+
+  // From useInput hook
+  // const [enteredName, setEnteredName] = useState("");
+
   // used to not show errror in the starting
   // not corect way as enteredName is not valid to start with as its not even there
   // const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
@@ -10,17 +31,23 @@ const SimpleInput = (props) => {
   // so we need another state enteredNameTouched to handle this situation when the input field is untouched
   // const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   // we can get rid of this enteredNameIsValid state and derive a constant from other two states
-  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  // From useInput hook
+  // const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
   // const [formIsValid, setFormIsValid] = useState(false);
 
-  const [enteredEmail, setEnteredEmail] = useState("");
-  const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
+  // From useInput hook
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [enteredEmailTouched, setEnteredEmailTouched] = useState(false);
 
-  const enteredNameIsValid = enteredName.trim().length !== 0;
-  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+  // From useInput hook
+  // const enteredNameIsValid = enteredName.trim().length !== 0;
+  // const nameInputHasError = !enteredNameIsValid && enteredNameTouched;
 
-  const enteredEmailIsValid = enteredEmail.includes("@");
-  const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailTouched;
+  // From useInput hook
+  // const enteredEmailIsValid = enteredEmail.includes("@");
+  // const emailInputHasError = !enteredEmailIsValid && enteredEmailTouched;
 
   // useEffect just adds an extra re-evaluation cycle
   // we can use just derived constants here instead of setting setFormIsValid
@@ -39,39 +66,44 @@ const SimpleInput = (props) => {
     formIsValid = true;
   }
 
+  // From useInput hook
   // validate on every keystroke
-  const nameInputChangeHandler = (event) => {
-    setEnteredName(event.target.value);
+  // const nameInputChangeHandler = (event) => {
+  //   setEnteredName(event.target.value);
 
-    // updated value of enteredName may not be available just after  setEnteredName()
-    // if (enteredName.trim().length !== 0) {
-    // if (event.target.value.trim().length !== 0) {
-    //   setEnteredNameIsValid(true);
-    // }
-  };
+  //   // updated value of enteredName may not be available just after  setEnteredName()
+  //   // if (enteredName.trim().length !== 0) {
+  //   // if (event.target.value.trim().length !== 0) {
+  //   //   setEnteredNameIsValid(true);
+  //   // }
+  // };
 
-  const emailInputChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
-  };
+  // From useInput hook
+  // const emailInputChangeHandler = (event) => {
+  //   setEnteredEmail(event.target.value);
+  // };
 
+  // From useInput hook
   // validate on blur
-  const nameInputBlurHandler = (event) => {
-    setEnteredNameTouched(true);
-    // if (enteredName.trim().length === 0) {
-    //   setEnteredNameIsValid(false);
-    // }
-  };
+  // const nameInputBlurHandler = (event) => {
+  //   setEnteredNameTouched(true);
+  //   // if (enteredName.trim().length === 0) {
+  //   //   setEnteredNameIsValid(false);
+  //   // }
+  // };
 
-  const emailInputBlurHandler = (event) => {
-    setEnteredEmailTouched(true);
-  };
+  // From useInput hook
+  // const emailInputBlurHandler = (event) => {
+  //   setEnteredEmailTouched(true);
+  // };
 
   // validate on form submit
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
     // because if the user submits the form all the inputs are considered as touched
-    setEnteredNameTouched(true);
+    // commented out because we are not able to submit the form if there are errors
+    // setEnteredNameTouched(true);
 
     // if (enteredName.trim().length === 0) {
     //   setEnteredNameIsValid(false);
@@ -79,10 +111,10 @@ const SimpleInput = (props) => {
     // }
     // setEnteredNameIsValid(true);
 
-    // we want to keep this if to cancel the form submission if inout is not valid.
+    // we want to keep this if to cancel the form submission if input is not valid.
     // no need to check the validity
     // just check if enteredNameIsValid is false
-    if (!enteredNameIsValid) {
+    if (!enteredNameIsValid || !enteredEmailIsValid) {
       return;
     }
 
@@ -92,22 +124,29 @@ const SimpleInput = (props) => {
     // const enteredValue = nameInputRef.current.value;
     // console.log("enteredValue", enteredValue); // ref
 
-    setEnteredName(""); // clear input field with setState
+    // From useInput hook
+    // setEnteredName(""); // clear input field with setState
     // nameInputRef.current.value = ""; // clear input field with ref, not ideal to manipulate DOM
-    setEnteredNameTouched(false); // after the form is submitted, it should be set back to untouched
 
-    setEnteredEmail("");
-    setEnteredEmailTouched(false);
+    // From useInput hook
+    // setEnteredNameTouched(false); // after the form is submitted, it should be set back to untouched
+
+    resetNameInput();
+    resetEmailInput();
+
+    // From useInput hook
+    // setEnteredEmail("");
+    // setEnteredEmailTouched(false);
   };
 
   // const nameInputClasses = enteredNameIsValid
   //  ? "form-control"
   //   : "form-control invalid";
-  const nameInputClasses = nameInputIsInvalid
+  const nameInputClasses = nameInputHasError
     ? "form-control invalid"
     : "form-control";
 
-  const emailInputClasses = emailInputIsInvalid
+  const emailInputClasses = emailInputHasError
     ? "form-control invalid"
     : "form-control";
 
@@ -125,7 +164,7 @@ const SimpleInput = (props) => {
           onBlur={nameInputBlurHandler}
         />
         {/* {!enteredNameIsValid && ( */}
-        {nameInputIsInvalid && (
+        {nameInputHasError && (
           <p className="error-text">Name must not be empty</p>
         )}
       </div>
@@ -140,7 +179,7 @@ const SimpleInput = (props) => {
           onBlur={emailInputBlurHandler}
         />
         {/* {!enteredNameIsValid && ( */}
-        {emailInputIsInvalid && (
+        {emailInputHasError && (
           <p className="error-text">Please enter a valid email</p>
         )}
       </div>
---
+++ src/hooks/use-input.js
@@ -0,0 +1,35 @@
+// hook to manage state and logic for an input.
+// here i wnat to manage the value of an input, the touch state, infer its validity (concrete validation should be passed form outside)
+import { useState } from "react";
+const useInput = (validateValue) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+
+  // const valueIsValid = enteredValue.trim() !== "";
+  const valueIsValid = validateValue(enteredValue);
+  const hasError = !valueIsValid && isTouched;
+
+  const valueChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const inputBlurHandler = (event) => {
+    setIsTouched(true);
+  };
+
+  const reset = () => {
+    setEnteredValue("");
+    setIsTouched(false);
+  };
+
+  return {
+    value: enteredValue,
+    isValid: valueIsValid,
+    hasError,
+    valueChangeHandler,
+    inputBlurHandler,
+    reset,
+  };
+};
+
+export default useInput;
